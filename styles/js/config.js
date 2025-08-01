c348732c4b67bf44939caab98d08f1703fb3aa59
Proutum-Ann <154941331+Proutum-Ann@users.noreply.github.com>
2025-08-01 21:16:58 +0000

    edited search and added TH linking

--- styles/js/config.js
+++ styles/js/config.js
@@ -42,12 +42,13 @@ charadex.sheet = {
 
   options: {
 
-    designTypes: ['All', 'Official Design', 'Guest Design', 'MYO Slot', 'MYO Design'],
-    statuses: ['All', 'Resell', 'Trade', 'Gift', 'Voided', 'For Sale', 'Purchased'],
-    rarity: ['All', 'Common', 'Uncommon', 'Rare', 'Very Rare', 'Legendary'],
-    species: ['All', 'Dog', 'Cat', 'Bunny'],
+    designTypes: ['All', 'NPC Design', 'Official Design', 'Guest Design', 'MYO Slot', 'MYO Design'],
+    statuses: ['All', 'Resell', 'Trade', 'Gift', 'Voided', 'For Sale', 'Purchased', 'Not for Sale'],
+    rarity: ['All', 'Common', 'Uncommon', 'Rare', 'Legendary'],
+    species: ['All', 'Hearthood'],
     itemTypes: ['All', 'Currency', 'MYO Slot', 'Pet', 'Trait', 'Misc'],
-    traitTypes: ['All', 'Ears', 'Eyes', 'Body', 'Limbs', 'Tails', 'Misc', 'Mutations']
+    traitTypes: ['All', 'Hood', 'Ears', 'Paws', 'Limbs', 'Tails', 'Misc'],
+    subspecies: ['All', 'Smoldanian', 'Omniscrawna', 'Looskedee', 'Novarian'],
 
   }
 
